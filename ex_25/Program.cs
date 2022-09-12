f0a92137ec2785c65794d4e89ae1add560318ad0
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-12 13:28:15 +0300

    add hometask 25 Raising To The Power

---
+++ ex_25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Input numbers A B: ");
+string numbers = Console.ReadLine()!;
+string[] parts = numbers.Split(' ');
+int A = int.Parse(parts[0]);
+int B = int.Parse(parts[1]);
+
+int RaisToThePower(int A, int B)
+{
+    int ApowB = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        ApowB = A * ApowB;
+    }
+    return ApowB;
+}
+
+Console.WriteLine(RaisToThePower(A, B));
+
+
+

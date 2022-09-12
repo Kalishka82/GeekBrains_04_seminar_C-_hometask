c817935040c4c95f443766d58e9c57778ceea370
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-12 15:36:06 +0300

    add hometask 29 Create And Print Array

---
+++ ex_29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+
+Console.Write("Input numbers through the space: ");
+string numbers = Console.ReadLine()!;
+string[] elements = numbers.Split(' ');
+int size = elements.Length;
+
+int[] arr = CreateArray(size);
+PrintArray(arr);  // PrintArray(CreateArray(8));
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = int.Parse(elements[i])!;
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    // Console.Write ($"[{string.Join (", ", arr)}]");
+    Console.WriteLine("]");
+}

82e6d5ba783bc0573ac40dd8431e459e6f1081ea
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-12 13:38:09 +0300

    add hometask 27 Sum Of Number Digits

---
+++ ex_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+Console.Write("Input any integer number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int SumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumOfDigits(number));
